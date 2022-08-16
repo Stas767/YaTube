349f1e01a96d450dd0914f7cf829b541e8aa76f5
Станислав Балджи <stanislavbaldzhy.yandex.ru>
2022-08-16 12:37:05 +0400

    Исправления по pep8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Из модуля models импортируем модель Post
-from .models import Post, Group, Comment,Follow
+from .models import Post, Group, Comment, Follow
 
 
 class PostAdmin(admin.ModelAdmin):
