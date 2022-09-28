cba1f2efe5913706f5c74ed1bf56ca030479f0e9
Станислав Балджи <stanislavbaldzhy.yandex.ru>
2022-09-28 17:47:31 +0400

    Добавил настройки в settings для деплоя

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -114,6 +114,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 
